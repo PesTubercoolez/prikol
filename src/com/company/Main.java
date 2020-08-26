64eaa8559f41f76cf3b82360cf75e8eaa287249b
PesTubercoolez <70197903+PesTubercoolez@users.noreply.github.com>
2020-08-26 14:10:31 +0300

    v.0.2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("lokust");
+	int earthMass = 105;
+	double gForceEarth= 9.8;
+	double massOnMoon;
+	double clearWeight;
+	double gForceMoon;
+
+	clearWeight = earthMass / gForceEarth;
+	gForceMoon = gForceEarth * 0.17;
+	massOnMoon = gForceMoon * clearWeight;
+	System.out.println("Your weight on Moon is equal to " + massOnMoon);
     }
 }
